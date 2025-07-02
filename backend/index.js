1d13ad1308c94d19af722cf0f9377c4a590b36e1
DomitoryProd <domitoryprod@gmail.com>
2025-07-02 22:39:58 +0500

    Demo hud is working. Basic score, player stats based on viewed player.

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
 
-// Create WebSocket server attached to the Express server
+// WebSocket server attached to the Express server listening to frontend connections on port 3000
 const server = app.listen(3000, () => {
     console.log('Express server is running on port 3000');
 });
@@ -15,17 +15,15 @@ const server = app.listen(3000, () => {
 const wss = new WebSocket.Server({ server });
 console.log("WebSocket server is live!");
 
-var frontendconnected = 0;
 
 wss.on("connection", ws => {
-    frontendconnected++;
-    console.log("Frontend number: " + frontendconnected + " has connected!");
+    console.log("Frontend has connected!");
+    //ws.send(JSON.stringify(gameState));
     ws.on("message", data => {
         console.log(data);
     });
     ws.on("close", ()=>{
-        console.log("Frontend number: " + frontendconnected + " has disconnected!");
-        frontendconnected--;
+        console.log("Frontend disconnected!");
     });
 });
 
@@ -35,7 +33,6 @@ app.post('/cs2', (req, res) => {
         let scorect = req.body && req.body.map ? req.body.map.team_ct.score : 0;
         let scoret = req.body && req.body.map ? req.body.map.team_t.score : 0;
         console.log(req.body);
-        //console.log("CT Score: " + scorect + " T Score: " + scoret);
         // Create a message object to send
         const message = req.body;
         // Send the message to all connected WebSocket clients
